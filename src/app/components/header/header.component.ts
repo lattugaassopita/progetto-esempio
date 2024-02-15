b853d2903257ec986bed62c71d2b9ecc1aea7f0d
Anita <anitastrawberrymiao@gmail.com>
2024-02-15 16:20:44 +0100

    update header with searchbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor() {}
+  
+  ngOnInit() {
+    this.navbarIsNotVisible = true;
+  }
+
+
+  navbarIsNotVisible: boolean = false;
+
+  showNavbar() {
+    this.navbarIsNotVisible = !this.navbarIsNotVisible;
+    console.log("adesso navbarIsNotVisible è", this.navbarIsNotVisible)
+  }
+
+  resetNavbar() {
+    this.navbarIsNotVisible= true;
+  }
 
 }
