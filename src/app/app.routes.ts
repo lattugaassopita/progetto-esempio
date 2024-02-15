33d2afd52b8e3dd604827e513485c246e5c5ddde
Anita <anitastrawberrymiao@gmail.com>
2024-02-15 23:17:28 +0100

    update header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CreateEventComponent } from './pages/create-event/create-event.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "home", pathMatch: 'full'},
 
     {path: "home", component: HomeComponent},
-    {path: "login", component: LoginComponent}
+    {path: "login", component: LoginComponent},
+    {path: "createEvent", component: CreateEventComponent}
 ];
