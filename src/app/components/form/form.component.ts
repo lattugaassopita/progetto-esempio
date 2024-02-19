98cc2d1ef955ef391a03471d466b716037a7e71f
Anita <anitastrawberrymiao@gmail.com>
2024-02-19 16:04:18 +0100

    update form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterModule } from '@angular/router';
@@ -5,14 +6,19 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule, RouterModule],
+  imports: [FormsModule, RouterModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+event: Event =  {eventName: '', eventBio: '', eventPrice: 0, eventOrg: false}
+
 
   sendForm(form:NgForm) {
-    console.log("sendForm() is working")
+    console.log("sendForm() is working");
+    console.log(form.value);
   }
 
 }
+
+type Event = {eventId?: string, eventName: string, eventBio?: string, eventPrice: number, eventDate?: Date, eventPic?: File, eventOrg: boolean}
