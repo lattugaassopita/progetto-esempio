97fab786f7784b6cc5bbfed84e47b25f99d3d849
Anita <anitastrawberrymiao@gmail.com>
2024-02-28 15:36:26 +0100

    update carousel view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { CreateEventComponent } from './pages/create-event/create-event.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "home", pathMatch: 'full'},
 
     {path: "home", component: HomeComponent},
     {path: "login", component: LoginComponent},
-    {path: "createEvent", component: CreateEventComponent}
+    {path: "createEvent", component: CreateEventComponent},
+    
+    {path: "**", component: ErrorPageComponent}
 ];
