a5c9afa2d6cbfe5c474bdf03fcdab169b32d676d
Anita <anitastrawberrymiao@gmail.com>
2024-03-01 18:56:37 +0100

    update header with graphic bug

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, MenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  navbarIsOff: boolean = true;
+  propicNotClicked: boolean = true;
+
+  clickOnTheMagnifyingGlass() {
+    this.navbarIsOff=!this.navbarIsOff;
+    console.log("is the navbar off?", this.navbarIsOff)
+  }
+
+  clickOnThePropic() {
+    this.propicNotClicked=!this.propicNotClicked;
+    console.log("Did you cliked on the propic?", this.propicNotClicked);
+  }
 }
