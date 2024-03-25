347c89eb957a15ee9a05cc6b1aaa020676194bbb
Anita <anitastrawberrymiao@gmail.com>
2024-03-25 18:09:33 +0100

    update form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+
+  ciao() {
+    console.log("NgSubmit is working");
+  }
 
 }
